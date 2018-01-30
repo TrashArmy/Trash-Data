b324feb3cda779acdf009568e6eacb9c6012fbcf
Mr-sagarmandal <sagar187@hotmail.com>
2018-01-29 19:04:50 -0500

    Added light.py for white light

---
+++ light.py
@@ -0,0 +1,37 @@
+import time
+from neopixel import *
+import sys
+
+# LED strip configuration:
+LED_COUNT      = 64      # Number of LED pixels.
+LED_PIN        = 18      # GPIO pin connected to the pixels (18 uses PWM!).
+#LED_PIN        = 10      # GPIO pin connected to the pixels (10 uses SPI /dev/spidev0.0).
+LED_FREQ_HZ    = 800000  # LED signal frequency in hertz (usually 800khz)
+LED_DMA        = 10      # DMA channel to use for generating signal (try 10)
+LED_BRIGHTNESS = 255     # Set to 0 for darkest and 255 for brightest
+LED_INVERT     = False   # True to invert the signal (when using NPN transistor level shift)
+LED_CHANNEL    = 0       # set to '1' for GPIOs 13, 19, 41, 45 or 53
+LED_STRIP      = ws.WS2811_STRIP_GRB   # Strip type and colour ordering
+
+# Define functions which animate LEDs in various ways.
+def colorWipe(strip, color, wait_ms=50):
+    """Wipe color across display a pixel at a time."""
+    for i in range(strip.numPixels()):
+        strip.setPixelColor(i, color)
+        strip.show()
+        #time.sleep(wait_ms/1000.0)
+
+if __name__ == '__main__':
+    # Create NeoPixel object with appropriate configuration.
+    strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL, LED_STRIP)
+    # Intialize the library (must be called once before other functions).
+    strip.begin()
+    
+    print('Lighting NeoPixel...')
+    try:
+        while True:
+            colorWipe(strip, Color(255, 255, 255)) # White wipe
+    except (KeyboardInterrupt):
+        colorWipe(strip, Color(0,0,0))
+        sys.exit(0)
+
