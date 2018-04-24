cb2c3c19b7ae055446930c1ace0aa931d5f4dd16
Stephen Brotman <brotman.stephen@gatech.edu>
2018-04-24 00:40:42 +0000

    Added more pictures

---
+++ Image-Capture/switch.py
@@ -0,0 +1,62 @@
+"""Switch controller
+"""
+
+import pigpio
+import time
+
+
+class Switch:
+    def __init__(self, pi, pin_switch):
+        self.pi = pi
+
+        if not self.pi.connected:
+            exit()
+
+        self.pin_switch = pin_switch
+        self.pi.set_mode(self.pin_switch, pigpio.INPUT)
+        self.pi.set_pull_up_down(self.pin_switch, pigpio.PUD_DOWN)
+
+        self._prevprev = 0
+        self._prev = 0
+        self._curr = 0
+
+    def read(self):
+        tmp = self.pi.read(self.pin_switch)
+
+        # Only update value if read value is different from old current value
+        if (self._curr != tmp):
+            self._prevprev = self._prev
+            self._prev = self._curr
+            self._curr = tmp
+
+        return self._curr
+
+    def toggled(self):
+        self.read()
+        if (self._prevprev == self._curr and self._prevprev != self._prev):
+            if (self._curr == 0):
+                # Reset to prevent multiple toggles
+                self._prevprev = self._prev
+                #return 1
+                return False
+            elif (self._curr == 1):
+                # Reset to prevent multiple toggles
+                self._prevprev = self._prev
+
+                return 2
+            else:
+                return False
+        else:
+            return False
+
+if __name__ == '__main__':
+    pi = pigpio.pi()
+
+    s = Switch(pi, 26)
+    try:
+        while(True):
+            print(s.read())
+            print(s.toggled())
+            time.sleep(0.25)  # 100 ms
+    except (KeyboardInterrupt):
+        pi.stop()
